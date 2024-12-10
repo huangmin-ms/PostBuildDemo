a6ce0f19a81251b03146b6f6ea63e6645ba4e066
huangmin-ms <huangmin@microsoft.com>
2024-12-10 18:54:50 +0800

    Add comments

--- PostBuildDemo/Program.cs
+++ PostBuildDemo/Program.cs
@@ -1,7 +1,14 @@
 ﻿namespace PostBuildDemo
 {
+    /// <summary>
+    /// The main program class.
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// The entry point for the application.
+        /// </summary>
+        /// <param name="args">args</param>
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
